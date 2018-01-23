9da68735023f43b700907d4560f9e29de45b8909
Neil Mathew <mathew.neil@gmail.com>
2018-01-23 16:52:10 +0530

    Add original website

---
+++ js/splash-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	// Calling our splashScreen plugin and
+	// passing an array with images to be shown
+	
+	$('#promoIMG').splashScreen({
+		textLayers : [
+			'img/splash/beautifullysimple.png',
+			'img/splash/simplybeautiful.png',
+			'img/splash/metalwihen.png'
+		]
+	});
+	
+});
